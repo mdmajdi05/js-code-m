60ad24bb42007e511d24abcc76ae0f252784548e
mdmajdi05 <mdmajdi05@gmail.com>
2024-11-06 12:33:45 +0000

    container config done

---
+++ .devcontainer/basic/advanced js/script.js
@@ -0,0 +1,15 @@
+//const btn1 = document.querySelector('.content')
+
+//function chngclr() {
+   
+ //  btn1.style.backgroundColor="red"
+//};
+
+//const btn2 = document.querySelector('.content')
+
+//function stopchng() {
+ 
+  
+//}
+ console.log(Math.random());
+ 
